16e302277b1a7246212e09798a1093e131a1f4c8
Juliana  Guerrero <140461034+julianag27@users.noreply.github.com>
2025-09-23 18:11:45 -0400

    Added a toggle button to be able to see the password

--- apps/web/src/frontend/App.tsx
+++ apps/web/src/frontend/App.tsx
@@ -1,15 +1,34 @@
-import './App.css'
+import React, { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const [showPassword, setShowPassword] = useState(false);
 
   return (
     <div className="login-container">
       <h1>MOBILE FINANCERS</h1>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <button>Log In</button>
-    </div>
-  )
-}
 
-export default App
+      <input type="text" placeholder="Username" />
+
+      <div className="password-wrapper">
+        <input
+          type={showPassword ? "text" : "password"}
+          placeholder="Password"
+        />
+        <button
+          type="button"
+          className="toggle-btn"
+          onClick={() => setShowPassword(!showPassword)}
+        >
+          {showPassword ? <FaEyeSlash /> : <FaEye />}
+        </button>
+      </div>
+
+      <button className="login-btn">Log In</button>
+    </div>
+  );
+};
+
+export default App;
+
